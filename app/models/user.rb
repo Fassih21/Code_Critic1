80b1b5c8dc377f276786c03954e1784e3f05a4e3
Fassih21 <fassihkamran880@gmail.com>
2026-03-19 16:29:38 +0500

    Edited User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,26 @@
 class User < ApplicationRecord
-  
   has_many :projects, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   before_save :normalize_name
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: :true
-  validates :password, format: {
-    with: /\A
-    (?=.*[a-z])
-    (?=.*[A-Z])
-    (?=.*\d)
-    (?=.*[!@#$%^&*])
-    /x,
-    message: "must include at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*)"
-  } if:->{ password.present? }
-  
-     def normalize_name
+  validates :email, presence: true, uniqueness: true
+
+  validates :password,
+    length: { minimum: 8 },
+    format: {
+      with: /\A
+        (?=.*[a-z])
+        (?=.*[A-Z])
+        (?=.*\d)
+        (?=.*[^A-Za-z0-9])
+      /x,
+      message: "must include uppercase, lowercase, number, and symbol"
+    },
+    if: -> { password.present? }
+
+  def normalize_name
     self.name = name.strip.titleize if name.present?
   end
 
