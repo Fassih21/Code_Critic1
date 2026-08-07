661b10a9b9c46d0767127c07c8b9a5734a469276
Fassih <fassihkamran880@gmail.com>
2026-08-08 00:30:36 +0530

    Added: Project test file

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Project, type: :model do
+
+    describe "associations" do
+        it {should belong_to(:user)}
+        it {should have_many(:code_files).dependent(:destroy)}
+    end
+
+    describe "validations" do
+        it {should validate_presence_of(:name)}
+        it {should validate_presence_of(:description)}
+    end
+end
